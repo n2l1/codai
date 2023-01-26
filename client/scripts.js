767c7c208f9691dd021b8e2a21f80522cbc86392
Nthuloane <thapelonthu@live.com>
2023-01-26 17:23:01 +0200

    update the server URL

--- client/scripts.js
+++ client/scripts.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
 
     // fetch data from the server - > bot's response
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codai-s4j5.onrender.com/', {
         method:'POST',
         headers: {
             'Content-Type': 'application/json'
